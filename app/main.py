3d9e3aab0a69ad4c385586cf49bc257c487c32d2
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-08-19 14:27:43 -0400

    added basic logging with external config

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
+import json
+import logging.config
 from fastapi import FastAPI
 
 from app.api import api
 
+with open('logging_config.json', 'r') as f:
+    config = json.load(f)
+
+logging.config.dictConfig(config)
+logger = logging.getLogger('sampleApp')
 
 app = FastAPI()
 app.include_router(api.router)
@@ -9,10 +16,10 @@ app.include_router(api.router)
 
 @app.on_event("startup")
 async def startup_event():
-    print("Starting application...")
+    logger.info("Starting application...")
 
 
 @app.on_event("shutdown")
 async def shutdown_event():
-    print("Shutting down application...")
+    logger.info("Shutting down application...")
 
